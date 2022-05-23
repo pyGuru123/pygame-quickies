1762d6c7a16404a3e1acdec3622948c102746b84
pyguru123 <pyguru123@gmail.com>
2022-05-23 22:59:11 +0530

    added aabb rect collision

---
+++ Collision Detection/aabb_rect.py
@@ -0,0 +1,9 @@
+# AABB collision detection.
+
+# Axis aligned bounding box - this means a collision detection between
+# 2 rectangles whose axis are aligned. means their is no rotation.
+
+import pygame
+
+pygame.init()
+WIDTH, HEIGHT = 288, 512
